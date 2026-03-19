213674f3d44991db1ce7270302b5f0e7014b4777
Kaneki07 <direlrodriguez@gmail.com>
2026-03-19 16:05:53 -0400

    Solucion a manejo de errores en la KEY de firebase

--- src/plugins/firebaseAdmin.js
+++ src/plugins/firebaseAdmin.js
@@ -4,6 +4,9 @@ import { createRequire } from "module"; // Necesario para importar JSON en ES Mo
 const require = createRequire(import.meta.url);
 const serviceAccount = require("../../serviceAccountKey.json"); // 👈 Ruta a tu archivo descargado
 
+// AGREGA ESTA LÍNEA PARA CORREGIR LOS SALTOS DE LÍNEA
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
